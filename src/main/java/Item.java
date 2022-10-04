2ddf6af55a82bdf697f4dc4d1caf523a440027a5
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-10-04 17:03:58 +0200

    Adding item class

---
+++ src/main/java/Item.java
@@ -0,0 +1,14 @@
+import fr.unice.polytech.cod.Recipe;
+
+import java.util.Queue;
+
+public class Item {
+    private Recipe recipe;
+    private int quantity;
+
+
+    public Item(Recipe recipe, int quantity){
+        this.quantity = quantity;
+        this.recipe = recipe;
+    }
+}
