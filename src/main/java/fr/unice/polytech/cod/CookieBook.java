857fbc2b3efa013746f47c7f9489fafba9435de6
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-10-04 17:16:39 +0200

    Adding CookieBook class

---
+++ src/main/java/fr/unice/polytech/cod/CookieBook.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.cod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CookieBook {
+    private List<Recipe> recipes;
+
+    public CookieBook(){
+        recipes = new ArrayList<>();
+        // TODO : Add recipes here.
+    }
+}
