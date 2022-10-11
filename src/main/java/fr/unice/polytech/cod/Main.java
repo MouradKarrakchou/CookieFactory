5bd9ffa20f3d04af74848dc89e31cabc4316af7a
AyoubIMAMI <youbima17@gmail.com>
2022-10-11 16:36:08 +0200

    main

--- src/main/java/fr/unice/polytech/cod/Main.java
+++ src/main/java/fr/unice/polytech/cod/Main.java
@@ -15,9 +15,12 @@ public class Main {
         FidelityAccount fidelityAccount = new FidelityAccount();
         User user = new User(cookieBook, cart, fidelityAccount);
 
-        user.seeCatalogue();
+        System.out.println("\nCatalogue : \n" + user.seeCatalogue() + "\n");
+        System.out.println("\nUser choose a cookie\n");
         user.chooseCookies(new Cookie("cookieName", new Dough(), new Flavour(), new ArrayList<>()), 12);
+        System.out.println("\nCart content : \n");
         user.recapCart();
+        System.out.println("\nCart is validate : \n");
         user.validateCart();
     }
 }
