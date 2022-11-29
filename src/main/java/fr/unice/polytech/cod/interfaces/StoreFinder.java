e44adc4e607f481509f21eb804ee3b0252ca0f1a
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-11-29 15:56:03 +0100

    Je push

--- src/main/java/fr/unice/polytech/cod/interfaces/StoreFinder.java
+++ src/main/java/fr/unice/polytech/cod/interfaces/StoreFinder.java
@@ -1,11 +1,12 @@
 package fr.unice.polytech.cod.interfaces;
 
+import fr.unice.polytech.cod.data.StoreLocation;
 import fr.unice.polytech.cod.exceptions.InvalidStoreException;
 import fr.unice.polytech.cod.store.Store;
 
 import java.util.List;
 
 public interface StoreFinder {
-    List<Store> getStores();
-    Store findStore(String name) throws InvalidStoreException;
+    List<Store> getStores(StoreLocation storeLocation);
+    Store findStore(StoreLocation storeLocation, String name) throws InvalidStoreException;
 }
