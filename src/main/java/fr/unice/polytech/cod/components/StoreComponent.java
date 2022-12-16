03d539b6e13b5d2de76c7fff865bec0310e29d50
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-12-16 15:48:13 +0100

    Fixing StoreComponent

--- src/main/java/fr/unice/polytech/cod/components/StoreComponent.java
+++ src/main/java/fr/unice/polytech/cod/components/StoreComponent.java
@@ -1,7 +1,5 @@
 package fr.unice.polytech.cod.components;
 
-import fr.unice.polytech.cod.exceptions.CookieAlreadyExistingException;
-import fr.unice.polytech.cod.exceptions.NotMatchingCatalogRequirementException;
 import fr.unice.polytech.cod.food.Cookie;
 import fr.unice.polytech.cod.food.ingredient.Ingredient;
 import fr.unice.polytech.cod.interfaces.*;
@@ -15,23 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class StoreComponent implements StoreModifier, StoreAccessor {
     private final ChefAction chefAction;
     private final StockExplorer stockExplorer;
-    private final OrderStatesAction orderStatesAction;
 
     @Autowired
-    public StoreComponent(ChefAction chefAction, StockExplorer stockExplorer, OrderStatesAction orderStatesAction) {
+    public StoreComponent(ChefAction chefAction, StockExplorer stockExplorer) {
         this.chefAction = chefAction;
         this.stockExplorer = stockExplorer;
-        this.orderStatesAction = orderStatesAction;
     }
-
 
     @Override
     public void changeOpeningHour(Store store, TimeClock open, TimeClock close) {
@@ -41,7 +35,6 @@ public class StoreComponent implements StoreModifier, StoreAccessor {
             chefAction.updateSchedule(chef,store);
         }
     }
-
 
     /**
      * Return the cookies a store can make based on the stock of the store and the recipes in the store's cookie book
@@ -63,13 +56,13 @@ public class StoreComponent implements StoreModifier, StoreAccessor {
     }
 
     @Override
-    public void updateTaxes(Store store, Ingredient ingredient, double tax) {
-        store.getTaxes().put(ingredient, tax);
-    }
+    public boolean setTax(Store store, Ingredient ingredient, double newPrice) {
+        Optional<Ingredient> _stockIngredient = stockExplorer.findIngredient(store.getStock(), ingredient);
+        if(_stockIngredient.isEmpty())
+            return false;
 
-    @Override
-    public void setTaxes(Store store, Map<Ingredient, Double> taxes) {
-        store.setTaxes(taxes);
+        _stockIngredient.get().setPrice(newPrice);
+        return true;
     }
 
     @Override
--- src/main/java/fr/unice/polytech/cod/food/ingredient/Ingredient.java
+++ src/main/java/fr/unice/polytech/cod/food/ingredient/Ingredient.java
@@ -45,6 +45,10 @@ public class Ingredient implements Cloneable {
         return price;
     }
 
+    public void setPrice(double price){
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
