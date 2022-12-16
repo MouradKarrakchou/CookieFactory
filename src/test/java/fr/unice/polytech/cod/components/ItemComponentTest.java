a811174eff421b051f4694b46c3f33c7ad022f6c
AyoubIMAMI <youbima17@gmail.com>
2022-12-16 17:51:22 +0100

    Test

--- src/test/java/fr/unice/polytech/cod/components/ItemComponentTest.java
+++ src/test/java/fr/unice/polytech/cod/components/ItemComponentTest.java
@@ -1,23 +1,8 @@
 package fr.unice.polytech.cod.components;
 
-
-import fr.unice.polytech.cod.pojo.CookieBook;
-import fr.unice.polytech.cod.interfaces.ItemActions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ItemComponentTest {
 
-    @Autowired
-    private ItemActions itemActions;
-
-    @Test
-    public void testDeTest() {
-        CookieBook cookieBook = new CookieBook();
-        assertEquals(0, itemActions.getQuantity(new Item(cookieBook.getCookies().get(0), 6)));
-    }
 }
