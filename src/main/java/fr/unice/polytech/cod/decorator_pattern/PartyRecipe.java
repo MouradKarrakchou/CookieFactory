0bb66e9e5370b09cc32cf8c119ced6b91306359c
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-12-16 19:31:56 +0100

    a laide

--- src/main/java/fr/unice/polytech/cod/decorator_pattern/PartyRecipe.java
+++ src/main/java/fr/unice/polytech/cod/decorator_pattern/PartyRecipe.java
@@ -1,19 +1,23 @@
 package fr.unice.polytech.cod.decorator_pattern;
 
 import fr.unice.polytech.cod.food.Cookie;
+import fr.unice.polytech.cod.food.PartyCookie;
 import fr.unice.polytech.cod.food.ingredient.*;
+import io.cucumber.java.sl.In;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PartyRecipe {
+    static HashMap<Ingredient, Boolean> emptyMap = new HashMap<>();
     private static final Cookie birthdayCookie = new Cookie("BirthdayDough", new Dough("BirthdayDough", 3,3), new Flavour("CandyFlavour", 2, 2), new ArrayList<>(List.of(new Topping("Candy", 2, 4),new Topping("Chocolate", 2, 4))), new Mix(Mix.MixState.MIXED), new Cooking(Cooking.CookingState.CHEWY), 120);
-    public static PartyCookieComponent birthdayPartyCookieComponent = new PartyCookie(birthdayCookie, PartyCookieComponent.CookieSize.L) {
+    public static PartyCookie birthdayPartyCookieComponent = new PartyCookie(birthdayCookie, PartyCookie.CookieSize.L, "police", PartyCookie.Event.Retirement, emptyMap) {
     };
 
     private static final Cookie RetirementCookie = new Cookie("RetirementDough", new Dough("RetirementDough", 3,3), new Flavour("ChocolateFlavour", 2, 2), new ArrayList<>(List.of(new Topping("Chocolate", 2, 4),new Topping("Fruits", 2, 4))), new Mix(Mix.MixState.MIXED), new Cooking(Cooking.CookingState.CHEWY), 120);
-    public static PartyCookieComponent retirementPartyCookieComponent = new PartyCookie(RetirementCookie, PartyCookieComponent.CookieSize.XL);
+    public static PartyCookie retirementPartyCookieComponent = new PartyCookie(RetirementCookie, PartyCookie.CookieSize.XL, "princess", PartyCookie.Event.Retirement, emptyMap);
 
     private static final Cookie WeddingCookie = new Cookie("WeddingDough", new Dough("WeddingDough", 3,3), new Flavour("ChocolateFlavour", 2, 2), new ArrayList<>(List.of(new Topping("Chocolate", 2, 4),new Topping("Fruits", 2, 4))), new Mix(Mix.MixState.MIXED), new Cooking(Cooking.CookingState.CHEWY), 120);
-    public static PartyCookieComponent weddingPartyCookieComponent = new PartyCookie(WeddingCookie, PartyCookieComponent.CookieSize.XL);
+    public static PartyCookie weddingPartyCookieComponent = new PartyCookie(WeddingCookie, PartyCookie.CookieSize.XL, "chat", PartyCookie.Event.Retirement, emptyMap);
 }
