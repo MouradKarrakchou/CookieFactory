0ee5dbd5973c8e378b812783c3937b15397a90ea
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-12-16 10:31:19 +0100

    refractoring to spring boot

--- src/main/java/fr/unice/polytech/cod/interfaces/OrderActions.java
+++ src/main/java/fr/unice/polytech/cod/interfaces/OrderActions.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.cod.interfaces;
 
+import fr.unice.polytech.cod.food.ingredient.Ingredient;
 import fr.unice.polytech.cod.order.Order;
 import fr.unice.polytech.cod.order.OrderState;
 import fr.unice.polytech.cod.user.User;
@@ -7,16 +8,13 @@ import fr.unice.polytech.cod.user.fidelityAccount.Discount;
 
 import java.time.Instant;
 import java.util.Optional;
+import java.util.Set;
 
 public interface OrderActions {
-    // Setter
-    void cancelOrder(Order order);
-    void updateState(Order order, OrderState newState);
 
     // Getter
     User getUser(Order order);
     boolean isCanceledTwiceInARow(Order order, Instant time);
     double computeTotalPrice(Order order);
-    OrderState getOrderState(Order order);
     Optional<Discount> getDiscount(Order order);
 }
--- src/main/java/fr/unice/polytech/cod/interfaces/OrderStatesAction.java
+++ src/main/java/fr/unice/polytech/cod/interfaces/OrderStatesAction.java
@@ -2,11 +2,19 @@ package fr.unice.polytech.cod.interfaces;
 
 import fr.unice.polytech.cod.order.Bill;
 import fr.unice.polytech.cod.order.Order;
+import fr.unice.polytech.cod.order.OrderState;
 import fr.unice.polytech.cod.store.Store;
 
 import java.util.List;
 
-public interface OrderStates {
+public interface OrderStatesAction {
+    // Setter
+    void cancelOrder(Order order);
+    void updateState(Order order, OrderState newState);
+
+
+
     public void retrieveOrder(List<Order> orderList,Bill bill) throws Exception;
+    OrderState getOrderState(Order order);
 
 }
