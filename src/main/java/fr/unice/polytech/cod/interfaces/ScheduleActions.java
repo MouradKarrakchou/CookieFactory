2a94e269943191181d51164d61e723bb50f7951d
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-12-17 21:24:03 +0100

    fixing tests

--- src/main/java/fr/unice/polytech/cod/interfaces/ScheduleActions.java
+++ src/main/java/fr/unice/polytech/cod/interfaces/ScheduleActions.java
@@ -10,16 +10,6 @@ import java.util.Optional;
 
 public interface ScheduleActions {
 
-    /**
-     * Give the available time slots of the Chef
-     *
-     * @param schedule schedule
-     * @param minutes minutes
-     * @param numberOfDaysBeforeTheOrder number of days
-     * @return the list of available Interval
-     */
-    List<Interval> getIntervals(Schedule schedule, int minutes, int numberOfDaysBeforeTheOrder);
-
     DaySlot getDaySlot(Schedule schedule, int numberOfDaysBefore);
 
     Optional<Order> getOrderToPrepare(Schedule schedule, int numberOfDaysBeforeTheOrder, TimeClock timeClock);
