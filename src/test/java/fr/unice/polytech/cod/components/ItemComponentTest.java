9c0a6f06fcf940ab1f67e65fae28d5ab8aa36dda
Emile Derain <emilederain@gmail.com>
2022-12-17 09:57:51 +0100

    Modif syntaxe

--- src/test/java/fr/unice/polytech/cod/components/ItemComponentTest.java
+++ src/test/java/fr/unice/polytech/cod/components/ItemComponentTest.java
@@ -1,8 +1,32 @@
 package fr.unice.polytech.cod.components;
 
+import fr.unice.polytech.cod.food.ingredient.Ingredient;
+import fr.unice.polytech.cod.interfaces.IngredientActions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ItemComponentTest {
+
+    @Autowired
+    IngredientActions ingredientActions;
+
+    Ingredient ingredient;
+
+    @BeforeEach
+    void init() {
+        this.ingredient = new Ingredient("Sucre", 1, 0);
+    }
+
+
+    @Test
+    void test() {
+        ingredientActions.increaseQuantity(this.ingredient, 10);
+        assertEquals(10, this.ingredient.getQuantity());
+    }
 
 }
