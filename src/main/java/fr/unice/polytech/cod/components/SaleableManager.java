0b3ca138b847495ef5a65bf2776cac895ebec7c2
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-12-17 21:46:06 +0100

    c'est bizarre ca marche 2

--- src/main/java/fr/unice/polytech/cod/components/SaleableManager.java
+++ src/main/java/fr/unice/polytech/cod/components/SaleableManager.java
@@ -8,6 +8,7 @@ import fr.unice.polytech.cod.pojo.ingredient.Topping;
 import fr.unice.polytech.cod.pojo.store.Store;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 @Component
@@ -32,14 +33,25 @@ public class SaleableManager implements Saleable {
         return price;
     }
 
-    private Set<Ingredient> getIngredientsCookie(Store store, Cookie cookie){
+    /**
+     * Give the set of all ingredients cloned needed to produce one cookie base on its recipe.
+     *
+     * @return The set of all ingredients cloned needed.
+     */
+    @Override
+    public Set<Ingredient> getIngredients(Cookie cookie){
         Set<Ingredient> ingredients = new HashSet<>();
-
         if (cookie.getDough() != null) ingredients.add(cookie.getDough().clone());
         if (cookie.getFlavour() != null) ingredients.add(cookie.getFlavour().clone());
         cookie.getToppingList().forEach(topping -> ingredients.add(topping.clone()));
         return ingredients;
     }
+
+    @Override
+    public ArrayList<Ingredient> getIngredientsList(Cookie cookie) {
+        return new ArrayList<>(getIngredients(cookie).stream().toList());
+    }
+
 
     private double getPriceForPartyCookie(Store store, PartyCookie partyCookie){
         double priceHT = getPriceForCookie(store, partyCookie);
